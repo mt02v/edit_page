ac7d8f7d23dae06b194b185c995871d351f4ca7b
mt02v <141305346+mt02v@users.noreply.github.com>
2024-02-03 14:35:02 +0000

    [Add] 日記投稿サービスを作る#２

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,20 @@ class PostsController < ApplicationController
 def index
   @posts = Post.all
 end
+
+def new
+    @post = Post.new
+end
+
+def create
+    @post = post.new(post_params)
+    
+    if @post.save
+      redirect_to posts_path
+    end
+end
+
+  def posta_params
+  params.require(:post).permit(:body,:title)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 get 'posts', to: 'posts#index'
+get 'posts/new', to: 'posts#new', as: 'new_post'
+post 'posts/', to: 'posts#create'
 end
