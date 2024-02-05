54c1586e6dfa2f9655c7709cd3b379730c4a7d43
mt02v <141305346+mt02v@users.noreply.github.com>
2024-02-05 14:25:59 +0000

    [Add] edit/update　コードの意味がわかる #4

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,18 +9,28 @@ end
 
 def create
     @post = post.new(post_params)
-    
+
     if @post.save
       redirect_to posts_path
     end
 end
-    
+
 def show
     @post = Post.find(params[:id])
+end
+
+def edit
+  @post = Post.find(params[:id])
+end
+
+def update
+  @post = Post.find(params[:id])
+  @post = @post.update(post_params)
+  redirect_to posts_path
 end
 
   def posta_params
   params.require(:post).permit(:body,:title)
   end
-  
+
 end
